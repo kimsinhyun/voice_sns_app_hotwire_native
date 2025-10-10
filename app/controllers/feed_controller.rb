a97b6479865205c3ea3cc99fdd2a492adc28fef5
sinhyun Kim <ksh415622@naver.com>
2025-10-10 20:03:13 +0900

    wip

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,7 @@
 class FeedController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def index
+    # user_agent = UserAgent.parse(request.user_agent)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,6 @@ Rails.application.routes.draw do
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
+
+  match "*path", to: redirect("/"), via: :all
 end
