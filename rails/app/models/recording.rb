e2ad3eec86b6a5ab7178d1dd3f54a5f5f92034ef
sinhyun Kim <ksh415622@naver.com>
2025-10-16 14:13:45 +0900

    wip

--- rails/app/models/recording.rb
+++ rails/app/models/recording.rb
@@ -3,4 +3,11 @@ class Recording < ApplicationRecord
   has_one_attached :audio_file
 
   # validates :audio_file, presence: true
+
+  def self.create_mock_data
+    user = User.last
+    recording = Recording.create!(user: user)
+    file_path = Rails.root.join("app", "assets", "audios", "sample_audio.mp3")
+    recording.audio_file.attach(io: File.open(file_path), filename: "sample_audio.mp3", content_type: "audio/mpeg")
+  end
 end
